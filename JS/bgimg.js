f6924ac5395606e46d4d343c89cd067421d60944
hoo091221 <96910225+hoo091221@users.noreply.github.com>
2024-05-10 13:49:35 +0900

    240510 - Main Update

---
+++ JS/bgimg.js
@@ -0,0 +1,32 @@
+function doesFileExist(urlToFile) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('HEAD', urlToFile, false);
+	xhr.send();
+	if (xhr.status == '404') {
+		return false;
+	} else {
+		return true;
+	}
+	var xhr = null;
+}
+
+function bg() {
+	var num = 1;
+	while (doesFileExist('./img/bgimg/bgimg_' + num + '.png') == true) {
+		num = ++num;
+	}
+	return num;
+}
+
+bg();
+
+var num = bg() - 1;
+document.getElementById('bgdiv').style.backgroundImage = "url('./img/bgimg/bgimg_" + num + ".png')";
+
+var num = bg() - 2;
+document.getElementById('bgdiv-1').style.backgroundImage = "url('./img/bgimg/bgimg_" + num + ".png')";
+
+var num = bg() - 3;
+document.getElementById('bgdiv-2').style.backgroundImage = "url('./img/bgimg/bgimg_" + num + ".png')";
+
+// astrogistmonamegistos
