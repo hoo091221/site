8e2545cd988562aa481af0e1b7a27dee24775a23
hoo091221 <96910225+hoo091221@users.noreply.github.com>
2024-06-26 11:43:01 +0900

    240626 - Bug Fix

--- JS/include.js
+++ JS/include.js
@@ -1,18 +1,16 @@
 // include.js
-function include() {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function(el) {
-        var includePath = `https://${window.location.host}/${el.dataset.includePath}`;
-        if (includePath) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.outerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', includePath, true);
-        }
-    });
-}
 
-include()
+var allElements = document.getElementsByTagName('*');
+Array.prototype.forEach.call(allElements, function(el) {
+    var includePath = `https://hoo091221.github.io/${el.dataset.includePath}`;
+    if (includePath) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                el.outerHTML = this.responseText;
+            }
+        };
+        xhttp.open('GET', includePath, true);
+        xhttp.send();
+    }
+});
